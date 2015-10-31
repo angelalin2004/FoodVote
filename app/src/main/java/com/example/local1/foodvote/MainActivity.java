7f8d947258c9e4629fcf15b32ebf45430b1ad0e9
Krushi <cubskru@gmail.com>
2015-10-30 22:13:06 -0700

    Preliminary API Usage working

--- app/src/main/java/com/example/local1/foodvote/MainActivity.java
+++ app/src/main/java/com/example/local1/foodvote/MainActivity.java
@@ -1,15 +1,45 @@
 package com.example.local1.foodvote;
 
 import android.os.Bundle;
+import android.os.Looper;
+import android.os.Handler;
+
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    /*
+     * Update OAuth credentials below from the Yelp Developers API site:
+     * http://www.yelp.com/developers/getting_started/api_access
+     */
+    private static final String CONSUMER_KEY = "FQFe1MpY3PGvxKy-Aq702g";
+    private static final String CONSUMER_SECRET = "u_ifYEaonk6W5sf24SCXiGPKx6I";
+    private static final String TOKEN = "6YSX1I448VpE2WQ1rrQv0NJRNJ9E9rOX";
+    private static final String TOKEN_SECRET = "_iN4GhZsdgojn1WYZTHOi-q2jzM";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +56,74 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        /*Create a YelpAPI Instance*/
+        YelpAPI yAPI = new YelpAPI(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET);
+
+        /*Call Helper method to get results of API Call in JSON Format*/
+        String JSONOutput = setUpAPIRets(yAPI, "La Jolla, CA");
+
+        /*Parse the JSON Format String*/
+        JSONParser parser = new JSONParser();
+        JSONObject response = null;
+        try {
+            response = (JSONObject) parser.parse(JSONOutput);
+        } catch (ParseException pe) {
+            System.out.println("Error: could not parse JSON response:");
+            System.out.println(JSONOutput);
+            System.exit(1);
+        }
+
+        /*Each buisness is now represented as an array entry*/
+        JSONArray businesses = (JSONArray) response.get("businesses");
+
+        /*FOR NOW MANUALLY PICK THE 1st 5 RESTUARANTS*/
+        JSONObject Buisness1 = (JSONObject) businesses.get(0);
+        JSONObject Buisness2 = (JSONObject) businesses.get(1);
+        JSONObject Buisness3 = (JSONObject) businesses.get(2);
+        JSONObject Buisness4 = (JSONObject) businesses.get(3);
+        JSONObject Buisness5 = (JSONObject) businesses.get(4);
+
+        /*GET THE NAME OF EACH RESTUARANT*/
+        String R1 = Buisness1.get("name").toString();
+        String R2 = Buisness2.get("name").toString();
+        String R3 = Buisness3.get("name").toString();
+        String R4 = Buisness4.get("name").toString();
+        String R5 = Buisness5.get("name").toString();
+
+        /*GET EACH TEXT VIEW BOX*/
+        TextView tv1 = (TextView)findViewById(R.id.TextView1);
+        TextView tv2 = (TextView)findViewById(R.id.TextView2);
+        TextView tv3 = (TextView)findViewById(R.id.TextView3);
+        TextView tv4 = (TextView)findViewById(R.id.TextView4);
+        TextView tv5 = (TextView)findViewById(R.id.TextView5);
+
+        /*SET EACH EXT VIEW BOX TO THE RESTUARANT NAME*/
+        tv1.setText(R1);
+        tv2.setText(R2);
+        tv3.setText(R3);
+        tv4.setText(R4);
+        tv5.setText(R5);
+
+
+
+    }
+
+    /*Method gets the API call in JSON format*/
+    public String setUpAPIRets(YelpAPI Y, String City){
+        /*To Return*/
+        String Ret = "";
+
+        try {
+            /*Get the return from the API call*/
+            Ret = Y.execute(City).get();
+        }
+        catch(Exception e){
+            System.out.print("Hi");
+        }
+
+        Log.println(Log.ERROR, "MAIN:", "result = " + Ret);
+        return Ret;
     }
 
     @Override
@@ -49,4 +147,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
+
--- app/src/main/java/com/example/local1/foodvote/YelpAPI.java
+++ app/src/main/java/com/example/local1/foodvote/YelpAPI.java
@@ -11,8 +11,16 @@ import org.scribe.model.Token;
 import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
 
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+import android.app.Activity;
+
+import android.app.Activity;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+
+import java.net.URL;
 
 /**
  * Code sample for accessing the Yelp API V2.
@@ -25,12 +33,12 @@ import com.beust.jcommander.Parameter;
  * See <a href="http://www.yelp.com/developers/documentation">Yelp Documentation</a> for more info.
  *
  */
-public class YelpAPI {
+public class YelpAPI extends AsyncTask<String, Void, String> {
 
     private static final String API_HOST = "api.yelp.com";
     private static final String DEFAULT_TERM = "dinner";
     private static final String DEFAULT_LOCATION = "San Francisco, CA";
-    private static final int SEARCH_LIMIT = 3;
+    private static final int SEARCH_LIMIT = 10;
     private static final String SEARCH_PATH = "/v2/search";
     private static final String BUSINESS_PATH = "/v2/business";
 
@@ -46,6 +54,8 @@ public class YelpAPI {
     OAuthService service;
     Token accessToken;
 
+    String [] results;
+    String res = "TOMATO";
     /**
      * Setup the Yelp API OAuth credentials.
      *
@@ -59,8 +69,63 @@ public class YelpAPI {
                 new ServiceBuilder().provider(TwoStepOAuth.class).apiKey(consumerKey)
                         .apiSecret(consumerSecret).build();
         this.accessToken = new Token(token, tokenSecret);
+        results = new String [5];
     }
 
+
+
+    /****************/
+
+    /*USE ASYNCTASK To run the API call on a non-UI/main thread. API Calls do not work on the main thread*/
+
+    /*Before Execution. As of now this isn't doing anything useful*/
+    @Override
+    protected void onPreExecute(){
+        Log.println(Log.ERROR, "YELPAPI:", "IN OnPreExecute!");
+        res = "GARFIELD";
+        //try{ Thread.sleep(10000); }catch(InterruptedException e){ }
+    }
+
+    /*The actual API Call process*/
+    @Override
+    protected String doInBackground(String...cities){
+
+        Log.println(Log.ERROR, "YELPAPI:", "IN doInBackground!");
+
+        /*Search for Buisnesses in the passed city parameter*/
+        String APIRes = searchForBusinessesByLocation("restuarant", cities[0]);
+        Log.println(Log.ERROR, "YELPAPI:", "Search Passed!");
+        /*Activity.runOnUiThread(new Runnable() {
+            public void run() {
+                results[0] = APIRes;
+            }
+        });*/
+
+        //results[0] = APIRes;
+        //return APIRes;
+        Log.println(Log.ERROR, "YELPAPI:", "Result = " + APIRes);
+
+        /*Return the JSON result of the API Call*/
+        return APIRes;
+    }
+
+    /*After the Execution. As of now not doing anything useful*/
+    @Override
+    protected void onPostExecute(String result){
+        super.onPostExecute(result);
+        Log.println(Log.ERROR, "YELPAPI:", "IN OnPostExec!");
+
+        //TextView tv1 = (TextView)findViewById(R.id.TextView1);
+        //tv1.setText(result);
+        res = result;
+
+        //return result;
+    }
+
+    /*NOT BEING USED*/
+    public String getResults(){
+        return res;
+    }
     /**
      * Creates and sends a request to the Search API by term and location.
      * <p>
@@ -72,10 +137,13 @@ public class YelpAPI {
      * @return <tt>String</tt> JSON Response
      */
     public String searchForBusinessesByLocation(String term, String location) {
+        Log.println(Log.ERROR, "YELPAPI:", "IN searchForBuisnessByLocation!");
         OAuthRequest request = createOAuthRequest(SEARCH_PATH);
+        Log.println(Log.ERROR, "YELPAPI:", "Made OAuthRequest!");
         request.addQuerystringParameter("term", term);
         request.addQuerystringParameter("location", location);
         request.addQuerystringParameter("limit", String.valueOf(SEARCH_LIMIT));
+        Log.println(Log.ERROR, "YELPAPI:", "Passed searchForBuisnessByLocation!");
         return sendRequestAndGetResponse(request);
     }
 
@@ -106,14 +174,17 @@ public class YelpAPI {
 
     /**
      * Sends an {@link OAuthRequest} and returns the {@link Response} body.
-     *
+     *f
      * @param request {@link OAuthRequest} corresponding to the API request
      * @return <tt>String</tt> body of API response
      */
     private String sendRequestAndGetResponse(OAuthRequest request) {
+        Log.println(Log.ERROR, "YELPAPI:", "IN sendRequestAndGetResponse!");
         System.out.println("Querying " + request.getCompleteUrl() + " ...");
         this.service.signRequest(this.accessToken, request);
+        Log.println(Log.ERROR, "YELPAPI:", "Trying to send request!");
         Response response = request.send();
+        Log.println(Log.ERROR, "YELPAPI:", "Returning from sendRequestAndGetResponse!");
         return response.getBody();
     }
 
@@ -162,18 +233,14 @@ public class YelpAPI {
         public String location = DEFAULT_LOCATION;
     }
 
-    /**
-     * Main entry for sample Yelp API requests.
-     * <p>
-     * After entering your OAuth credentials, execute <tt><b>run.sh</b></tt> to run this example.
-     */
-    public static void main(String[] args) {
-        YelpAPICLI yelpApiCli = new YelpAPICLI();
-        new JCommander(yelpApiCli, args);
+     /*NOT DOING ANYTHING RIGHTNOW*/
+     //public String execute(YelpAPI yelpApi){
+        //YelpAPICLI yelpApiCli = new YelpAPICLI();
+        //new JCommander(yelpApiCli, args);
 
-        YelpAPI yelpApi = new YelpAPI(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET);
-        queryAPI(yelpApi, yelpApiCli);
+        //YelpAPI yelpApi = new YelpAPI(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET);
+        //queryAPI(yelpApi, yelpApiCli);
 
-        yelpApi.searchForBusinessesByLocation("restaurant", "San Francisco, CA");
-    }
+    //    return yelpApi.searchForBusinessesByLocation("restaurant", "San Francisco, CA");
+    //}
 }
